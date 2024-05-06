ededa9cf96fa37a9308dda06a9bfac79875cc3b2
licyafreoli <168486966+licyafreoli@users.noreply.github.com>
2024-05-06 18:54:53 -0300

    versao 0.0.1
    aula 1 js

---
+++ aula1/aa.js
@@ -0,0 +1,26 @@
+// var name = prompt("qual seu nome?")
+// var age = prompt("qual a sua idade?")
+
+// let sorvete = {
+
+// name: name,
+// age: age,
+// isHuman: true
+// }
+
+// console.log(`olá, ${sorvete.name} você tem ${sorvete.age} anos`)
+
+let licya = [
+    'girafa',
+    'zebra',
+    17,
+    'alfabeto',
+    
+    { 
+      name: `alicia`,
+      age: 18,
+      isAdm: true
+    },
+    true
+    ]
+    console.log(`a ${licya[4].name} tem ${licya[2]} anos`)
